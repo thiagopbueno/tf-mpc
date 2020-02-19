a79d2890019c1e8ba887374cf49210e40df734b7
Thiago P. Bueno <thiago.pbueno@gmail.com>
2020-02-19 15:44:00 -0300

    chore(lqr): add URL reference to LQR algorithm
    Signed-off-by: Thiago P. Bueno <thiago.pbueno@gmail.com>

--- tfmpc/solvers/lqr/__init__.py
+++ tfmpc/solvers/lqr/__init__.py
@@ -1,3 +1,10 @@
+"""
+Linear Quadratic Regulator (LQR):
+
+Please see http://rail.eecs.berkeley.edu/deeprlcourse/static/slides/lec-10.pdf
+for notation and more details on LQR.
+"""
+
 import tensorflow as tf
 
 import numpy as np
