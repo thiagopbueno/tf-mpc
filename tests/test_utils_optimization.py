10ebe92f5e617bcb8af1fc6d115f298121881ee1
Thiago P. Bueno <thiago.pbueno@gmail.com>
2020-05-06 19:05:54 -0300

    feat: add projected_newton_qp solver in utils/optimization.py
    Signed-off-by: Thiago P. Bueno <thiago.pbueno@gmail.com>

---
+++ tests/test_utils_optimization.py
@@ -0,0 +1,79 @@
+import pytest
+import tensorflow as tf
+
+from tfmpc.utils import optimization
+
+
+@pytest.fixture(params=[
+    ([0.0, 0.0], [-1.0, 0.5], [1.0, 1.0], [0.0, 0.5]),
+    ([0.0, 0.0], [0.5, -1.0], [1.0, 1.0], [0.5, 0.0]),
+    ([1.0, 1.0], [0.0, 1.5], [2.0, 2.0], [1.0, 1.5]),
+    ([1.0, 1.0], [1.5, 0.0], [2.0, 2.0], [1.5, 1.0]),
+
+    ([0.0, 0.0, 0.0], [-1.0, 0.5, -1.0], [1.0, 1.0, 1.0], [0.0, 0.5, 0.0]),
+    ([0.0, 0.0, 0.0], [-1.0, 0.5, 0.30], [1.0, 1.0, 1.0], [0.0, 0.5, 0.30]),
+])
+def qp(request):
+    goal, low, high, x_star = request.param
+
+    dim = len(goal)
+
+    H = 2 * tf.eye(dim)
+    q = -2 * tf.constant(goal, shape=(dim, 1))
+    low = tf.constant(low, shape=(dim, 1))
+    high = tf.constant(high, shape=(dim, 1))
+    x_star = tf.constant(x_star, shape=(dim, 1))
+
+    return H, q, low, high, x_star
+
+
+def test_get_qp_indices(qp):
+    H, q, low, high, x_star = qp
+
+    for _ in range(10):
+        x = tf.random.uniform(
+            tf.shape(x_star), minval=low + 1e-4, maxval=high - 1e-4)
+
+        g = q + tf.matmul(H, x)
+        f, c = optimization._get_qp_indices(g, low, high, x)
+
+        assert tf.reduce_all(f == tf.constant([[True]] * len(x)))
+        assert tf.reduce_all(c == tf.constant([[False]] * len(x)))
+
+
+def test_get_newton_step(qp):
+    H, q, low, high, x_star = qp
+
+    for _ in range(10):
+        x = tf.random.uniform(tf.shape(x_star), minval=low, maxval=high)
+
+        g = q + tf.matmul(H, x)
+        f, c = optimization._get_qp_indices(g, low, high, x)
+
+        delta_x = tf.Variable(tf.zeros_like(x))
+        g_f = optimization._get_newton_step(H, q, x, f, c, delta_x)
+
+
+def test_projected_newton_qp(qp):
+    H, q, low, high, x_star = qp
+
+    x, _, _ = optimization.projected_newton_qp(H, q, low, high, x_star)
+    assert tf.reduce_all(x == x_star)
+
+    for _ in range(10):
+        x_0 = tf.random.uniform(tf.shape(x_star), minval=low, maxval=high)
+
+        x, _, _ = optimization.projected_newton_qp(H, q, low, high, x_0)
+        assert tf.reduce_all(x == x_star)
+
+        for i in range(x_0.shape[0]):
+            x = tf.Variable(x_0, trainable=False)
+            x[i].assign(low[i])
+            x, _, _ = optimization.projected_newton_qp(H, q, low, high, x)
+            assert tf.reduce_all(x == x_star)
+
+        for i in range(x_0.shape[0]):
+            x = tf.Variable(x_0, trainable=False)
+            x[i].assign(high[i])
+            x, _, _ = optimization.projected_newton_qp(H, q, low, high, x)
+            assert tf.reduce_all(x == x_star)
---
+++ tfmpc/utils/optimization.py
@@ -0,0 +1,83 @@
+import tensorflow as tf
+
+
+def projected_newton_qp(H, q, low, high, x, eps=1e-6, alpha_0=1.0, rho=0.5, c=1e-4):
+
+    def f(x):
+        quadratic = tf.matmul(x, tf.matmul(H, x), transpose_a=True)
+        linear = tf.matmul(q, x, transpose_a=True)
+        return tf.squeeze(1 / 2 * quadratic + linear)
+
+    while True:
+        g = q + tf.matmul(H, x)
+        free, clamped = _get_qp_indices(g, low, high, x)
+
+        delta_x = tf.Variable(tf.zeros_like(x))
+        g_f = _get_newton_step(H, q, x, free, clamped, delta_x)
+
+        if tf.norm(g_f) < eps:
+            break
+
+        alpha = _armijo_line_search(f, x, g, delta_x, alpha_0, rho, c)
+        x = tf.clip_by_value(x + alpha * delta_x, low, high)
+
+    return x, free, clamped
+
+
+def _armijo_line_search(f, x, g, delta_x, alpha_0=1.0, rho=0.5, c=1e-4):
+    grad = tf.squeeze(tf.matmul(g, delta_x, transpose_a=True))
+
+    alpha = alpha_0
+
+    while True:
+        lhs = f(x + alpha * delta_x)
+        rhs = f(x) + c * alpha * grad
+
+        if lhs <= rhs:
+            break
+
+        alpha *= rho
+
+    return alpha
+
+
+@tf.function
+def _get_qp_indices(g, low, high, x, eps=1e-6):
+    c = tf.logical_or(
+            tf.logical_and(tf.abs(x - low) < eps, g > 0),
+            tf.logical_and(tf.abs(high - x) < eps, g < 0))
+    f = tf.logical_not(c)
+    return f, c
+
+
+@tf.function
+def _get_newton_step(H, q, x, f, c, delta_x):
+    x_f = tf.expand_dims(x[f], -1)
+    x_c = tf.expand_dims(x[c], -1)
+
+    q_f = tf.expand_dims(q[f], -1)
+
+    f = tf.cast(f, tf.int32)
+    c = tf.cast(c, tf.int32)
+    dim_f = tf.math.count_nonzero(f)
+    dim_c = tf.math.count_nonzero(c)
+
+    ff = tf.cast(tf.matmul(f, f, transpose_b=True), tf.bool)
+    H_ff = tf.reshape(H[ff], [dim_f, dim_f])
+
+    fc = tf.cast(tf.matmul(f, c, transpose_b=True), tf.bool)
+    H_fc = tf.reshape(H[fc], [dim_f, dim_c])
+
+    g_f = q_f
+    if dim_f > 0:
+        g_f += tf.matmul(H_ff, x_f)
+    if dim_c > 0:
+        g_f += tf.matmul(H_fc, x_c)
+
+    delta_x_f = -tf.matmul(tf.linalg.inv(H_ff), g_f)
+    delta_x_f = tf.reshape(delta_x_f, [-1])
+
+    indices = tf.where(tf.cast(f, tf.bool))
+    delta_x.scatter_nd_update(indices, delta_x_f)
+
+    return g_f
