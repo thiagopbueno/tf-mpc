3df90277addef92769ce0688565e73b15e20adf0
Thiago P. Bueno <thiago.pbueno@gmail.com>
2020-05-12 13:39:05 -0300

    feat: add Navigation class (with deceleration zones)
    Signed-off-by: Thiago P. Bueno <thiago.pbueno@gmail.com>

--- tests/test_env_navigation.py
+++ tests/test_env_navigation.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 import tensorflow as tf
 
@@ -5,73 +6,103 @@ from tfmpc.envs import navigation
 
 
 @pytest.fixture
-def navlin():
-    goal = tf.constant([[8.0], [-9.0]])
-    beta = 5.0
-    return navigation.NavigationLQR(goal, beta)
+def env():
+    goal = tf.constant([[8.0], [9.0]])
+    deceleration = {
+        "center": tf.constant([[[4.0], [3.0]]]),
+        "decay": tf.constant([2.0])
+    }
+    low = [-1.0, -1.0]
+    high = [1.0, 1.0]
+    return navigation.Navigation(goal, deceleration, low, high)
 
 
-def test_transition(navlin):
+def test_deceleration(env):
+    center = env.deceleration["center"][0]
+    decay = env.deceleration["decay"][0]
+
+    for _ in range(10):
+        state = tf.random.normal(shape=[], mean=center, stddev=2.0)
+        lambda_ = env._deceleration(state)
+
+        distance = np.sqrt(np.sum((state - center) ** 2))
+        expected = 2 / (1.0 + np.exp(-decay * distance)) - 1.0
+
+        assert isinstance(lambda_, tf.Tensor)
+        assert lambda_.shape == []
+        assert lambda_.dtype == tf.float32
+        assert tf.abs(lambda_ - expected) < 1e-4
+
+
+def test_transition(env):
     for _ in range(5):
         x0 = tf.random.normal((2, 1))
         u0 = tf.random.uniform((2, 1))
 
-        x1 = navlin.transition(x0, u0)
+        lambda_ = env._deceleration(x0)
+        x1 = env.transition(x0, u0)
 
         assert isinstance(x1, tf.Tensor)
         assert x1.shape == x0.shape
-        assert all(x1 == x0 + u0)
+        assert all(x1 == x0 + lambda_ * u0)
 
 
-def test_linear_transition(navlin):
+def test_linear_transition(env):
+    goal = env.goal
+    decay = env.deceleration["decay"][0]
+    center = env.deceleration["center"][0]
+
     for _ in range(5):
-        x = tf.random.normal((2, 1))
+        x = tf.random.normal(shape=(2, 1), mean=center, stddev=2.0)
         u = tf.random.uniform((2, 1))
 
-        model = navlin.get_linear_transition(x, u)
+        distance = tf.norm(x - center)
+        lambda_ = env._deceleration(x)
+
+        model = env.get_linear_transition(x, u)
         f = model.f
         f_x = model.f_x
         f_u = model.f_u
 
         assert isinstance(f, tf.Tensor)
         assert f.shape == x.shape
-        assert tf.reduce_all(f == navlin.transition(x, u))
-
-        assert isinstance(f_x, tf.Tensor)
-        assert f_x.shape == [x.shape[0]] * 2
-        assert tf.reduce_all(f_x == tf.eye(2))
+        assert tf.reduce_all(f == env.transition(x, u))
 
         assert isinstance(f_u, tf.Tensor)
         assert f_u.shape == [u.shape[0]] * 2
-        assert tf.reduce_all(f_u == tf.eye(2))
+        assert tf.reduce_all(f_u == lambda_ * tf.eye(2))
+
+        lambda_x = (2 * decay * tf.exp(-decay * distance) / ((1 + tf.exp(-decay * distance)) ** 2) * (x - center) / distance)
+        expected_f_x = (tf.eye(tf.shape(x)[0])
+                        + tf.tensordot(tf.squeeze(u),
+                                       tf.squeeze(lambda_x), axes=0))
+        assert isinstance(f_x, tf.Tensor)
+        assert f_x.shape == [x.shape[0]] * 2
+        assert tf.reduce_all(tf.abs(f_x - expected_f_x) < 1e-4)
 
 
-def test_cost(navlin):
-    goal = navlin.goal
-    beta = navlin.beta
+def test_cost(env):
+    goal = env.goal
 
     for _ in range(5):
         x = tf.random.normal((2, 1))
         u = tf.random.uniform((2, 1))
 
-        c = navlin.cost(x, u)
+        c = env.cost(x, u)
 
         assert isinstance(c, tf.Tensor)
         assert c.shape == []
-        assert tf.reduce_all(
-            c == tf.reduce_sum((x - goal) ** 2) + beta * tf.reduce_sum(u ** 2)
-        )
+        assert tf.reduce_all(c == tf.reduce_sum((x - goal) ** 2))
 
 
-def test_quadratic_cost(navlin):
-    goal = navlin.goal
-    beta = navlin.beta
+def test_quadratic_cost(env):
+    goal = env.goal
 
     for _ in range(5):
         x = tf.random.normal((2, 1))
         u = tf.random.uniform((2, 1))
 
-        model = navlin.get_quadratic_cost(x, u)
+        model = env.get_quadratic_cost(x, u)
         l = model.l
         l_x = model.l_x
         l_u = model.l_u
@@ -82,47 +113,41 @@ def test_quadratic_cost(navlin):
 
         assert isinstance(l, tf.Tensor)
         assert l.shape == []
-        assert tf.reduce_all(l == navlin.cost(x, u))
+        assert tf.reduce_all(l == env.cost(x, u))
 
         assert isinstance(l_x, tf.Tensor)
-        assert l_x.shape == x.shape
         assert tf.reduce_all(l_x == 2 * (x - goal))
 
         assert isinstance(l_u, tf.Tensor)
-        assert l_u.shape == u.shape
-        assert tf.reduce_all(l_u == 2 * beta * u)
+        assert tf.reduce_all(l_u == tf.zeros_like(u))
 
         assert isinstance(l_xx, tf.Tensor)
-        assert l_xx.shape == [x.shape[0]] * 2
         assert tf.reduce_all(l_xx == 2 * tf.eye(2))
 
         assert isinstance(l_uu, tf.Tensor)
-        assert l_uu.shape == [u.shape[0]] * 2
-        assert tf.reduce_all(l_uu == 2 * beta * tf.eye(2))
+        assert tf.reduce_all(l_uu == tf.zeros([u.shape[0], u.shape[0]]))
 
         assert isinstance(l_ux, tf.Tensor)
-        assert l_xx.shape == (x.shape[0], u.shape[0])
         assert tf.reduce_all(l_ux == tf.zeros((x.shape[0], u.shape[0])))
 
         assert isinstance(l_xu, tf.Tensor)
-        assert l_xx.shape == (u.shape[0], x.shape[0])
         assert tf.reduce_all(l_xu == tf.zeros((u.shape[0], x.shape[0])))
 
 
-def test_quadratic_final_cost(navlin):
-    goal = navlin.goal
+def test_quadratic_final_cost(env):
+    goal = env.goal
 
     for _ in range(5):
         x = tf.random.normal((2, 1))
 
-        model = navlin.get_quadratic_final_cost(x)
+        model = env.get_quadratic_final_cost(x)
         l = model.l
         l_x = model.l_x
         l_xx = model.l_xx
 
         assert isinstance(l, tf.Tensor)
         assert l.shape == []
-        assert tf.reduce_all(l == navlin.final_cost(x))
+        assert tf.reduce_all(l == env.final_cost(x))
 
         assert isinstance(l_x, tf.Tensor)
         assert l_x.shape == x.shape
--- tfmpc/envs/navigation.py
+++ tfmpc/envs/navigation.py
@@ -5,6 +5,75 @@ import tensorflow as tf
 from tfmpc.envs.diffenv import DiffEnv
 
 
+class Navigation(DiffEnv):
+
+    def __init__(self, goal, deceleration, low, high):
+        self.goal = goal
+        self.deceleration = deceleration
+
+        self.obs_space = gym.spaces.Box(
+            low=np.array([-np.inf, -np.inf], dtype=np.float32),
+            high=np.array([np.inf, np.inf], dtype=np.float32),
+        )
+        self.action_space = gym.spaces.Box(
+            low=np.array(low, dtype=np.float32),
+            high=np.array(high, dtype=np.float32),
+        )
+
+    @property
+    def action_size(self):
+        return self.state_size
+
+    @property
+    def state_size(self):
+        return self.goal.shape[0]
+
+    @tf.function
+    def transition(self, state, action):
+        lambda_ = self._deceleration(state)
+        return state + lambda_ * action
+
+    @tf.function
+    def cost(self, state, action):
+        return tf.reduce_sum((state - self.goal) ** 2)
+
+    @tf.function
+    def final_cost(self, state):
+        return tf.reduce_sum((state - self.goal) ** 2)
+
+    @tf.function
+    def _deceleration(self, state):
+        center = self.deceleration["center"]
+        decay = self.deceleration["decay"]
+        distance = tf.norm(state - center)
+        lambdas = 2 / (1.0 + tf.exp(-decay * distance)) - 1.0
+        return tf.reduce_prod(lambdas,  axis=-1)
+
+    def __repr__(self):
+        goal = self.goal.numpy().squeeze().tolist()
+
+        low = self.action_space.low.squeeze().tolist()
+        high = self.action_space.high.squeeze().tolist()
+        bounds = f"[{low}, {high}]"
+
+        center = self.deceleration["center"].numpy().tolist()
+        decay = self.deceleration["decay"].numpy().tolist()
+        deceleration = f"{{center={center}, decay={decay}}}"
+
+        return f"Navigation(goal={goal}, deceleration={deceleration}, bounds={bounds})"
+
+    @classmethod
+    def load(cls, config):
+        goal = tf.constant(config["goal"])
+        low = config["low"]
+        high = config["high"]
+        deceleration = {
+            key: tf.constant(val)
+            for key, val in config["deceleration"].items()
+        }
+        return cls(goal, deceleration, low, high)
+
+
 class NavigationLQR(DiffEnv):
 
     def __init__(self, goal, beta, low=None, high=None):
@@ -16,10 +85,8 @@ class NavigationLQR(DiffEnv):
         if high is None:
             high = np.inf
 
+        self.obs_space = gym.spaces.Box(-np.inf, np.inf, shape=tf.shape(goal))
         self.action_space = gym.spaces.Box(low, high, shape=tf.shape(goal))
-
-        self.obs_space = gym.spaces.Box(
-            low=-np.inf, high=np.inf, shape=tf.shape(goal))
 
     @property
     def action_size(self):
