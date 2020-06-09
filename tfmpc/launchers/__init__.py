8691c848815d965ce27c8ec8227351494ec449d7
Thiago P. Bueno <thiago.pbueno@gmail.com>
2020-06-09 19:29:41 -0300

    feat: add launchers module to start solvers
    Signed-off-by: Thiago P. Bueno <thiago.pbueno@gmail.com>

---
+++ tfmpc/launchers/__init__.py
@@ -0,0 +1,50 @@
+import json
+import os
+
+import tensorflow as tf
+
+from tfmpc import agents
+from tfmpc import envs
+from tfmpc import runners
+from tfmpc.solvers import ilqr
+
+
+def ilqr_run(config):
+    env_config = config.pop("env")
+    with open(env_config, "r") as file:
+        env_config = json.load(file)
+
+    env = envs.make_env(env_config)
+
+    x0 = tf.constant(env_config["initial_state"], dtype=tf.float32)
+    T = tf.constant(config.pop("horizon"), dtype=tf.int32)
+
+    solver = ilqr.iLQR(env, **config)
+    trajectory, iterations = solver.solve(x0, T)
+
+    output = os.path.join(config["logdir"], "data.csv")
+    trajectory.save(output)
+
+    return env, trajectory
+
+
+def online_ilqr_run(config):
+    env_config = config.pop("env")
+    with open(env_config, "r") as file:
+        env_config = json.load(file)
+
+    env = envs.make_env(env_config)
+
+    x0 = tf.constant(env_config["initial_state"], dtype=tf.float32)
+    T = tf.constant(config.pop("horizon"), dtype=tf.int32)
+
+    solver = ilqr.iLQR(env, **config)
+    controller = agents.OnlineController(solver, T)
+
+    runner = runners.Runner(env, controller)
+    with runner(x0, T) as r:
+        trajectory = r.run()
+
+        output = os.path.join(config["logdir"], "data.csv")
+        trajectory.save(output)
+
