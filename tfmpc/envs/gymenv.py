57f0343f66bc57a75d9d2c770f890de1cf74c25f
Thiago P. Bueno <thiago.pbueno@gmail.com>
2020-06-09 22:08:58 -0300

    feat: add GymEnv wrapper class
    Signed-off-by: Thiago P. Bueno <thiago.pbueno@gmail.com>

---
+++ tfmpc/envs/gymenv.py
@@ -0,0 +1,40 @@
+import gym.core
+
+
+class GymEnv(gym.core.Env):
+
+    def __init__(self):
+        self._t = None
+        self._state = None
+        self._info = {}
+
+    def setup(self, initial_state, horizon):
+        self.initial_state = initial_state
+        self.horizon = horizon
+
+    def step(self, action):
+        next_state = self.transition(self._state, action, cec=False)
+        cost = self.cost(self._state, action)
+        done = (self._t == self.horizon - 1)
+        info = self._info
+
+        self._t += 1
+        self._state = next_state
+
+        return (next_state, cost, done, info)
+
+    def reset(self):
+        self._t = 0
+        self._state = self.initial_state
+        self._info = {}
+
+        return self._state
+
+    def render(self, mode="human"):
+        pass
+
+    def close(self):
+        pass
+
+    def seed(self, seed=None):
+        pass
