6cd88014bf73b5c33a179f0748264e1a7190b62e
Thiago P. Bueno <thiago.pbueno@gmail.com>
2020-06-24 10:45:24 -0300

    feat: handle gradients and jacobians in batch by default in DiffEnv class
    Signed-off-by: Thiago P. Bueno <thiago.pbueno@gmail.com>

--- tfmpc/envs/diffenv.py
+++ tfmpc/envs/diffenv.py
@@ -10,79 +10,67 @@ FinalCostApprox = namedtuple("CostApprox", "l l_x l_xx")
 
 class DiffEnv:
 
-    @tf.function
-    def get_linear_transition(self, state, action, batch=True):
+    @tf.function(input_signature=[
+        tf.TensorSpec(shape=[None, None, 1], dtype=tf.float32),
+        tf.TensorSpec(shape=[None, None, 1], dtype=tf.float32)
+    ])
+    def get_linear_transition(self, state, action):
+        cec =  tf.constant(True)
+
         with tf.GradientTape(persistent=True) as tape:
             tape.watch(state)
             tape.watch(action)
-            f = self.transition(state, action, batch)
+            f = self.transition(state, action, cec)
 
-        if batch:
-            f_x = tape.batch_jacobian(f, state)
-            f_u = tape.batch_jacobian(f, action)
-            f_x = tf.squeeze(f_x, axis=[2, 4])
-            f_u = tf.squeeze(f_u, axis=[2, 4])
-        else:
-            f_x, f_u = tape.jacobian(f, [state, action])
-            f_x = tf.squeeze(f_x)
-            f_u = tf.squeeze(f_u)
+        f_x = tape.batch_jacobian(f, state)
+        f_u = tape.batch_jacobian(f, action)
+        f_x = tf.squeeze(f_x, axis=[2, 4])
+        f_u = tf.squeeze(f_u, axis=[2, 4])
 
         del tape
 
         return TransitionApprox(f, f_x, f_u)
 
-    @tf.function
-    def get_quadratic_cost(self, state, action, batch=True):
+    @tf.function(input_signature=[
+        tf.TensorSpec(shape=[None, None, 1], dtype=tf.float32),
+        tf.TensorSpec(shape=[None, None, 1], dtype=tf.float32),
+    ])
+    def get_quadratic_cost(self, state, action):
         with tf.GradientTape(persistent=True) as tape:
             tape.watch(state)
             tape.watch(action)
-            l = self.cost(state, action, batch)
+            l = self.cost(state, action)
             l_x, l_u = tape.gradient(
                 l, [state, action],
                 unconnected_gradients=tf.UnconnectedGradients.ZERO)
 
-        if batch:
-            l_xx = tape.batch_jacobian(
-                l_x, state,
-                unconnected_gradients=tf.UnconnectedGradients.ZERO)
-            l_xu = tape.batch_jacobian(
-                l_x, action,
-                unconnected_gradients=tf.UnconnectedGradients.ZERO)
+        l_xx = tape.batch_jacobian(
+            l_x, state,
+            unconnected_gradients=tf.UnconnectedGradients.ZERO)
+        l_xu = tape.batch_jacobian(
+            l_x, action,
+            unconnected_gradients=tf.UnconnectedGradients.ZERO)
 
-            l_xx = tf.squeeze(l_xx, axis=[2, 4])
-            l_xu = tf.squeeze(l_xu, axis=[2, 4])
+        l_xx = tf.squeeze(l_xx, axis=[2, 4])
+        l_xu = tf.squeeze(l_xu, axis=[2, 4])
 
-        else:
-            l_xx, l_xu = tape.jacobian(
-                l_x, [state, action],
-                unconnected_gradients=tf.UnconnectedGradients.ZERO)
+        l_ux = tape.batch_jacobian(
+            l_u, state,
+            unconnected_gradients=tf.UnconnectedGradients.ZERO)
+        l_uu = tape.batch_jacobian(
+            l_u, action,
+            unconnected_gradients=tf.UnconnectedGradients.ZERO)
 
-            l_xx = tf.squeeze(l_xx)
-            l_xu = tf.squeeze(l_xu)
-
-        if batch:
-            l_ux = tape.batch_jacobian(
-                l_u, state,
-                unconnected_gradients=tf.UnconnectedGradients.ZERO)
-            l_uu = tape.batch_jacobian(
-                l_u, action,
-                unconnected_gradients=tf.UnconnectedGradients.ZERO)
-
-            l_ux = tf.squeeze(l_ux, axis=[2, 4])
-            l_uu = tf.squeeze(l_uu, axis=[2, 4])
-        else:
-            l_ux, l_uu = tape.jacobian(
-                l_u, [state, action],
-                unconnected_gradients=tf.UnconnectedGradients.ZERO)
-
-            l_ux = tf.squeeze(l_ux)
-            l_uu = tf.squeeze(l_uu)
+        l_ux = tf.squeeze(l_ux, axis=[2, 4])
+        l_uu = tf.squeeze(l_uu, axis=[2, 4])
 
         del tape
 
         return CostApprox(l, l_x, l_u, l_xx, l_uu, l_ux, l_xu)
 
-    @tf.function
+    @tf.function(input_signature=[
+        tf.TensorSpec(shape=[None, 1], dtype=tf.float32)
+    ])
     def get_quadratic_final_cost(self, state):
         with tf.GradientTape(persistent=True) as tape:
             tape.watch(state)
