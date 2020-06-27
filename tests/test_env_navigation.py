449f3177fd5db6b93c43c729674a257a12805b5f
Thiago P. Bueno <thiago.pbueno@gmail.com>
2020-06-26 21:23:29 -0300

    feat: handle batch tensors by default in Navigation class
    Signed-off-by: Thiago P. Bueno <thiago.pbueno@gmail.com>

--- tests/test_env_navigation.py
+++ tests/test_env_navigation.py
@@ -2,61 +2,86 @@ import numpy as np
 import pytest
 import tensorflow as tf
 
+from tests.conftest import sample_state, sample_action
+
 
 def test_deceleration(navigation):
     center = navigation.deceleration["center"]
     decay = navigation.deceleration["decay"]
 
-    for _ in range(10):
-        state = tf.random.normal(shape=[], mean=center[0], stddev=2.0)
-        lambda_ = navigation._deceleration(state, batch=False)
+    state1 = sample_state(navigation)
+    state2 = sample_state(navigation, batch_size=10)
 
-        expected = 1.0
-        for c, d in zip(center, decay):
-            delta = np.squeeze(state - c)
-            distance = np.sqrt(np.sum(delta ** 2, axis=-1))
-            expected *= 2 / (1.0 + np.exp(-d * distance)) - 1.0
+    for state in [state1, state2]:
+        batch_size = tf.shape(state)[0]
 
-        assert isinstance(lambda_, tf.Tensor)
-        assert lambda_.shape == []
-        assert lambda_.dtype == tf.float32
-        assert tf.abs(lambda_ - expected) < 1e-4
+        lambda_ = navigation._deceleration(state)
+        lambda_ = navigation._deceleration(state)
 
-
-def test_deceleration_batch(navigation):
-    center = navigation.deceleration["center"]
-    decay = navigation.deceleration["decay"]
-
-    batch_size = 5
-    for _ in range(10):
-        state = tf.random.normal(shape=[batch_size,],
-                                 mean=center[0], stddev=2.0)
-        state = tf.reshape(state, (batch_size, -1, 1))
-        lambda_ = navigation._deceleration(state, batch=True)
-
-        expected = 1.0
-        for c, d in zip(center, decay):
-            delta = np.squeeze(state - c)
-            distance = np.sqrt(np.sum(delta ** 2, axis=-1))
-            expected *= 2 / (1.0 + np.exp(-d * distance)) - 1.0
-
-        assert isinstance(lambda_, tf.Tensor)
         assert lambda_.shape == [batch_size,]
-        assert lambda_.dtype == tf.float32
-        assert tf.reduce_all(tf.abs(lambda_ - expected) < 1e-4)
+
+        for i in range(batch_size):
+            state_ = state[i]
+            lambda_i = lambda_[i]
+
+            expected = 1.0
+            for c, d in zip(center, decay):
+                delta = np.squeeze(state_ - c)
+                distance = np.sqrt(np.sum(delta ** 2, axis=-1))
+                expected *= 2 / (1.0 + np.exp(-d * distance)) - 1.0
+
+            assert isinstance(lambda_i, tf.Tensor)
+            assert lambda_i.shape == []
+            assert lambda_i.dtype == tf.float32
+            assert tf.reduce_all(tf.abs(lambda_i - expected) < 1e-4)
+
+    fn = navigation._deceleration
+    cfn1 = fn.get_concrete_function(state1)
+    cfn2 = fn.get_concrete_function(state2)
+    assert cfn1 == cfn2
 
 
 def test_transition(navigation):
-    for _ in range(10):
-        x0 = tf.random.normal((2, 1))
-        u0 = tf.random.uniform((2, 1))
+    state1 = sample_state(navigation)
+    action1 = sample_action(navigation)
 
-        lambda_ = navigation._deceleration(x0, batch=False)
-        x1 = navigation.transition(x0, u0, batch=False)
+    batch_size = 10
+    state2 = sample_state(navigation, batch_size=batch_size)
+    action2 = sample_action(navigation, batch_size=batch_size)
 
-        assert isinstance(x1, tf.Tensor)
-        assert x1.shape == x0.shape
-        assert all(x1 == x0 + lambda_ * u0)
+    for state, action in [(state1, action1), (state2, action2)]:
+        batch_size = tf.shape(state)[0]
+
+        lambda_ = navigation._deceleration(state)
+
+        cec = tf.constant(False)
+        next_state = navigation.transition(state, action, cec)
+        assert next_state.shape == state.shape
+
+        next_state2 = navigation.transition(state, action, cec)
+        assert tf.reduce_any(next_state != next_state2)
+
+        cec = tf.constant(True)
+        next_state = navigation.transition(state, action, cec)
+        assert next_state.shape == state.shape
+
+        next_state2 = navigation.transition(state, action, cec)
+        assert tf.reduce_all(next_state == next_state2)
+
+        for i in range(batch_size):
+            assert tf.reduce_all(next_state[i] == state[i] + lambda_[i] * action[i])
+
+    fn = navigation.transition
+
+    cec = tf.constant(True)
+    cfn1 = fn.get_concrete_function(state1, action1, cec)
+    cfn2 = fn.get_concrete_function(state2, action2, cec)
+
+    cec = tf.constant(False)
+    cfn3 = fn.get_concrete_function(state1, action1, cec)
+    cfn4 = fn.get_concrete_function(state2, action2, cec)
+
+    assert cfn1 == cfn2 == cfn3 == cfn4
 
 
 def test_linear_transition(navigation):
@@ -64,63 +89,100 @@ def test_linear_transition(navigation):
     decay = navigation.deceleration["decay"]
     center = navigation.deceleration["center"]
 
-    for _ in range(10):
-        x = tf.random.normal(shape=(2, 1), mean=center[0], stddev=2.0)
-        u = tf.random.uniform((2, 1))
+    state1 = sample_state(navigation)
+    action1 = sample_action(navigation)
 
-        lambda_ = navigation._deceleration(x)
+    batch_size = 10
+    state2 = sample_state(navigation, batch_size=batch_size)
+    action2 = sample_action(navigation, batch_size=batch_size)
 
-        model = navigation.get_linear_transition(x, u, batch=False)
+    for state, action in [(state1, action1), (state2, action2)]:
+        batch_size = tf.shape(state)[0]
+
+        lambda_ = navigation._deceleration(state)
+
+        model = navigation.get_linear_transition(state, action)
         f = model.f
         f_x = model.f_x
         f_u = model.f_u
 
-        assert isinstance(f, tf.Tensor)
-        assert f.shape == x.shape
-        assert tf.reduce_all(f == navigation.transition(x, u))
+        assert f.shape == state.shape
 
-        assert isinstance(f_u, tf.Tensor)
-        assert f_u.shape == [u.shape[0]] * 2
-        assert tf.reduce_all(f_u == lambda_ * tf.eye(2))
+        cec = tf.constant(True)
+        assert tf.reduce_all(f == navigation.transition(state, action, cec))
 
-        lambda_x = tf.zeros_like(x)
-        for c, d in zip(center, decay):
-            dist = tf.norm(x - c)
-            l = 2 / (1.0 + tf.exp(-d * dist)) - 1.0
+        assert f_u.shape == [batch_size] + [navigation.action_size] * 2
+        assert tf.reduce_all(f_u == tf.reshape(lambda_, [-1, 1, 1]) * tf.eye(2))
+        assert f_x.shape == [batch_size] + [navigation.state_size] * 2
 
-            h = 2 * d * tf.exp(-d * dist) / ((1 + tf.exp(-d * dist)) ** 2)
-            lambda_x += h * (x - c) / dist * lambda_ / l
+        for i in range(batch_size):
+            state_ = state[i]
+            action_ = action[i]
 
-        expected_f_x = (tf.eye(tf.shape(x)[0])
-                        + tf.tensordot(tf.squeeze(u),
-                                       tf.squeeze(lambda_x), axes=0))
-        assert isinstance(f_x, tf.Tensor)
-        assert f_x.shape == [x.shape[0]] * 2
-        assert tf.reduce_all(tf.abs(f_x - expected_f_x) < 1e-4)
+            lambda_x = tf.zeros_like(state_)
+            for c, d in zip(center, decay):
+                dist = tf.norm(state_ - c)
+                l = 2 / (1.0 + tf.exp(-d * dist)) - 1.0
+
+                h = 2 * d * tf.exp(-d * dist) / ((1 + tf.exp(-d * dist)) ** 2)
+                lambda_x += h * (state_ - c) / dist * lambda_[i] / l
+
+            expected_f_x = (tf.eye(navigation.state_size)
+                            + tf.tensordot(tf.squeeze(action_),
+                                           tf.squeeze(lambda_x), axes=0))
+
+            assert tf.reduce_all(tf.abs(f_x[i] - expected_f_x) < 1e-4)
+
+    fn = navigation.get_linear_transition
+    cfn1 = fn.get_concrete_function(state1, action1)
+    cfn2 = fn.get_concrete_function(state2, action2)
+    assert cfn1 == cfn2
 
 
 def test_cost(navigation):
     goal = navigation.goal
 
-    for _ in range(5):
-        x = tf.random.normal((2, 1))
-        u = tf.random.uniform((2, 1))
+    state1 = sample_state(navigation)
+    action1 = sample_action(navigation)
 
-        c = navigation.cost(x, u)
+    batch_size = 10
+    state2 = sample_state(navigation, batch_size=batch_size)
+    action2 = sample_action(navigation, batch_size=batch_size)
 
-        assert isinstance(c, tf.Tensor)
-        assert c.shape == []
-        assert tf.reduce_all(c == tf.reduce_sum((x - goal) ** 2))
+    for state, action in [(state1, action1), (state2, action2)]:
+        batch_size = tf.shape(state)[0]
+
+        cost = navigation.cost(state, action)
+
+        assert isinstance(cost, tf.Tensor)
+        assert cost.shape == [batch_size,]
+        assert tf.reduce_all(cost >= 0.0)
+
+        cost2 = navigation.cost(tf.expand_dims(goal, axis=0), action)
+        assert tf.reduce_all(cost2 == 0.0)
+
+    fn = navigation.cost
+    cfn1 = fn.get_concrete_function(state1, action1)
+    cfn2 = fn.get_concrete_function(state2, action2)
+    assert cfn1 == cfn2
 
 
 def test_quadratic_cost(navigation):
     goal = navigation.goal
 
-    for _ in range(5):
-        x = tf.random.normal((2, 1))
-        u = tf.random.uniform((2, 1))
+    state1 = sample_state(navigation)
+    action1 = sample_action(navigation)
 
-        model = navigation.get_quadratic_cost(x, u, batch=False)
+    batch_size = 10
+    state2 = sample_state(navigation, batch_size=batch_size)
+    action2 = sample_action(navigation, batch_size=batch_size)
+
+    for state, action in [(state1, action1), (state2, action2)]:
+        batch_size = tf.shape(state)[0]
+        state_size = navigation.state_size
+        action_size = navigation.action_size
+
+        model = navigation.get_quadratic_cost(state, action)
         l = model.l
         l_x = model.l_x
         l_u = model.l_u
@@ -129,48 +191,42 @@ def test_quadratic_cost(navigation):
         l_xu = model.l_xu
         l_ux = model.l_ux
 
-        assert isinstance(l, tf.Tensor)
-        assert l.shape == []
-        assert tf.reduce_all(l == navigation.cost(x, u))
+        assert l.shape == [batch_size,]
+        assert tf.reduce_all(l == navigation.cost(state, action))
 
-        assert isinstance(l_x, tf.Tensor)
-        assert tf.reduce_all(l_x == 2 * (x - goal))
+        assert tf.reduce_all(l_x == 2 * (state - goal))
 
-        assert isinstance(l_u, tf.Tensor)
-        assert tf.reduce_all(l_u == tf.zeros_like(u))
+        assert tf.reduce_all(l_u == tf.zeros_like(action))
 
-        assert isinstance(l_xx, tf.Tensor)
-        assert tf.reduce_all(l_xx == 2 * tf.eye(2))
+        assert tf.reduce_all(l_xx == tf.reshape(
+            tf.tile(2 * tf.eye(state_size), [batch_size, 1]),
+            [batch_size, state_size, state_size]))
 
-        assert isinstance(l_uu, tf.Tensor)
-        assert tf.reduce_all(l_uu == tf.zeros([u.shape[0], u.shape[0]]))
+        assert tf.reduce_all(l_uu == tf.zeros([batch_size, action_size, action_size]))
 
-        assert isinstance(l_ux, tf.Tensor)
-        assert tf.reduce_all(l_ux == tf.zeros((x.shape[0], u.shape[0])))
+        assert tf.reduce_all(l_ux == tf.zeros([batch_size, state_size, action_size]))
 
-        assert isinstance(l_xu, tf.Tensor)
-        assert tf.reduce_all(l_xu == tf.zeros((u.shape[0], x.shape[0])))
+        assert tf.reduce_all(l_xu == tf.zeros([batch_size, action_size, state_size]))
+
+    fn = navigation.get_quadratic_cost
+    cfn1 = fn.get_concrete_function(state1, action1)
+    cfn2 = fn.get_concrete_function(state2, action2)
+    assert cfn1 == cfn2
 
 
 def test_quadratic_final_cost(navigation):
     goal = navigation.goal
 
-    for _ in range(5):
-        x = tf.random.normal((2, 1))
+    state = sample_state(navigation)[0]
 
-        model = navigation.get_quadratic_final_cost(x)
-        l = model.l
-        l_x = model.l_x
-        l_xx = model.l_xx
+    model = navigation.get_quadratic_final_cost(state)
+    l = model.l
+    l_x = model.l_x
+    l_xx = model.l_xx
 
-        assert isinstance(l, tf.Tensor)
-        assert l.shape == []
-        assert tf.reduce_all(l == navigation.final_cost(x))
+    assert l.shape == []
+    assert tf.reduce_all(l == navigation.final_cost(state))
 
-        assert isinstance(l_x, tf.Tensor)
-        assert l_x.shape == x.shape
-        assert tf.reduce_all(l_x == 2 * (x - goal))
+    assert tf.reduce_all(l_x == 2 * (state - goal))
+    assert tf.reduce_all(l_xx == 2 * tf.eye(navigation.state_size))
 
-        assert isinstance(l_xx, tf.Tensor)
-        assert l_xx.shape == [x.shape[0]] * 2
-        assert tf.reduce_all(l_xx == 2 * tf.eye(2))
--- tfmpc/envs/navigation/__init__.py
+++ tfmpc/envs/navigation/__init__.py
@@ -15,9 +15,11 @@ class Navigation(DiffEnv, GymEnv):
         self.deceleration = deceleration
 
         self.obs_space = gym.spaces.Box(
-            low=np.array([-np.inf, -np.inf], dtype=np.float32),
-            high=np.array([np.inf, np.inf], dtype=np.float32),
+            shape=goal.shape,
+            low=-np.inf,
+            high=np.inf
         )
+
         self.action_space = gym.spaces.Box(
             low=np.array(low, dtype=np.float32),
             high=np.array(high, dtype=np.float32),
@@ -31,40 +33,50 @@ class Navigation(DiffEnv, GymEnv):
     def state_size(self):
         return self.goal.shape[0]
 
-    @tf.function
-    def transition(self, state, action, batch=False, cec=True):
-        lambda_ = self._deceleration(state, batch)
-        if batch:
-            lambda_ = tf.reshape(lambda_, [tf.shape(lambda_)[0], 1, 1])
+    @tf.function(input_signature=[
+        tf.TensorSpec(shape=[None, None, 1], dtype=tf.float32),
+        tf.TensorSpec(shape=[None, None, 1], dtype=tf.float32),
+        tf.TensorSpec(shape=None, dtype=tf.bool)
+    ])
+    def transition(self, state, action, cec):
+        lambda_ = self._deceleration(state)
+        lambda_ = tf.reshape(lambda_, [-1, 1, 1])
 
         p = state + lambda_ * action
 
         if cec:
             next_state = p
         else:
-            next_state = p + tf.random.truncated_normal(tf.shape(p), mean=0.0, stddev=0.2)
+            noise = tf.random.truncated_normal(tf.shape(p), mean=0.0, stddev=0.2)
+            next_state = p + noise
 
         return next_state
 
-    @tf.function
-    def cost(self, state, action, batch=False):
+    @tf.function(input_signature=[
+        tf.TensorSpec(shape=[None, None, 1], dtype=tf.float32),
+        tf.TensorSpec(shape=[None, None, 1], dtype=tf.float32)
+    ])
+    def cost(self, state, action):
         state = tf.squeeze(state, axis=-1)
         goal = tf.squeeze(self.goal)
         return tf.reduce_sum((state - goal) ** 2, axis=-1)
 
     @tf.function
-    def final_cost(self, state, batch=False):
+    def final_cost(self, state):
         state = tf.squeeze(state, axis=-1)
         goal = tf.squeeze(self.goal)
         return tf.reduce_sum((state - goal) ** 2, axis=-1)
 
-    @tf.function
-    def _deceleration(self, state, batch=False):
+    @tf.function(input_signature=[
+        tf.TensorSpec(shape=[None, None, 1], dtype=tf.float32)
+    ])
+    def _deceleration(self, state):
         center = self.deceleration["center"]
         decay = self.deceleration["decay"]
 
-        if batch:
-            state = tf.expand_dims(state, axis=1)
+        center = tf.expand_dims(center, axis=0)
+        decay = tf.expand_dims(decay, axis=0)
+        state = tf.expand_dims(state, axis=1)
 
         delta = state - center
         delta = tf.squeeze(delta, axis=-1)
